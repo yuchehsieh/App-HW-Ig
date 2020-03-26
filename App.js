3fbc1177aeb0b58fb0e048809202eaf7f8d5b664
Murphy Hsieh <murphyhsieh@leandev.io>
2020-03-26 14:45:13 +0800

    finish stage 1 hw

--- App.js
+++ App.js
@@ -1,19 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import {createStackNavigator} from '@react-navigation/stack';
+import MessageListScreen from './src/components/MessageListScreen';
+import PostListScreen from "./src/components/PostListScreen";
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const RootStack = createStackNavigator();
+const rootNavigationRef = React.createRef();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+        <NavigationContainer ref={rootNavigationRef}>
+            <RootStack.Navigator>
+                <RootStack.Screen name="PostListScreen" component={PostListScreen} options={{
+                    headerLeft: () => <Image source={require('./assets/Icons/Camera.png')}
+                                             style={{marginLeft: 15, marginBottom: 5}}/>,
+                    headerTitle: () => <Image source={require('./assets/Instagram.png')} style={{width: 100}}/>,
+                    headerRight: () => {
+                        return (
+                            <TouchableOpacity
+                                onPress={() => rootNavigationRef.current?.navigate('MessageListScreen')}>
+                                <Image source={require('./assets/Icons/Message.png')}
+                                       style={{marginRight: 15, marginBottom: 5}}/>
+                            </TouchableOpacity>
+                        )
+                    }
+                }}/>
+                <RootStack.Screen
+                    name="MessageListScreen"
+                    component={MessageListScreen}
+                    options={{
+                        title: 'qishan_yu',
+                        headerLeft: () => {
+                            return (
+                                <TouchableOpacity style={{marginLeft: 15}}
+                                                  onPress={() => rootNavigationRef.current?.navigate('PostListScreen')}>
+                                    <Ionicons name="ios-arrow-back" size={24}/>
+                                </TouchableOpacity>
+                            )
+                        },
+                        headerRight: () => {
+                            return (
+                                <View style={styles.row}>
+                                    <TouchableOpacity style={{marginRight: 15}}>
+                                        <FontAwesome name="video-camera" size={22}/>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <FontAwesome name="pencil-square-o" size={22}/>
+                                    </TouchableOpacity>
+                                </View>
+                            )
+                        }
+                    }}
+                />
+            </RootStack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+        paddingRight: 15,
+    }
 });
---
+++ src/components/MessageListScreen.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Dimensions, FlatList, Image, StyleSheet, Text, View} from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import MessageListData from '../json/messageList';
+
+const screenWidth = Math.round(Dimensions.get('window').width);
+
+const RenderItem = ({name, id, activeMessage, avatarUrl}) => {
+    return (
+        <View style={styles.singleChatContainer} key={id}>
+            <View style={styles.leftElementContainer}>
+                <Image source={{uri: avatarUrl}} style={{height: 45, width: 45}}/>
+                <View style={styles.nameContainer}>
+                    <Text>{name}</Text>
+                    <Text style={styles.activity}>{activeMessage}</Text>
+                </View>
+            </View>
+            <Feather name="camera" size={28} color={'grey'}/>
+        </View>
+    )
+};
+
+function MessageListScreen({route, navigation}) {
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={MessageListData}
+                renderItem={({item}) => (
+                    <RenderItem name={item.name} activeMessage={item.activeMessage} id={item.id}
+                                avatarUrl={item.avatarUrl}/>)}
+                keyExtractor={({id}) => id}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingTop: 15
+    },
+    singleChatContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: screenWidth,
+        paddingHorizontal: 15,
+        marginBottom: 20
+    },
+    leftElementContainer: {
+        flexDirection: 'row',
+    },
+    nameContainer: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginLeft: 10
+    },
+    activity: {
+        color: 'grey',
+        marginTop: 5
+    }
+});
+
+export default MessageListScreen;
